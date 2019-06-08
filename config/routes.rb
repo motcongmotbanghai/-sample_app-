1696deaab32459e965791a21f56cbd74b17a70ac
motcongmotbanghai <leducvinh.dtu@gmail.com>
2019-06-08 15:38:10 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get  '/signup',  to: 'users#new'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/about',   to: 'static_pages#about'
+  get  '/help',    to: 'static_pages#help'
+  get  '/home',    to: 'static_pages#home'
 	root 'static_pages#home'
 end
